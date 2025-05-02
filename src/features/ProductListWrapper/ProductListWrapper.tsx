6ec0d08a3d2ac2202c856914efa98a119f5ff535
Dmitriy Kislitsyn <dima.music1989@gmail.com>
2025-05-02 08:33:08 +0300

    redeploy

--- src/features/ProductListWrapper/ProductListWrapper.tsx
+++ src/features/ProductListWrapper/ProductListWrapper.tsx
@@ -3,7 +3,7 @@ import Error from '../../shared/ui/Error/Error';
 import { IProduct } from '../../entities/product/model/product';
 import ProductList from '../ProductList/ProductList';
 
-// обгортка в якій отримуємо дані із сервера. ззаду для правильної роботи компонента Suspense
+// обгортка в якій отримуємо дані із сервера. ззаду для правильної роботи компонента Suspense.
 const ProductListWrapper = async () => {
 	// надсилання запиту для отримання продуктiв. Використовую звичайний fetch запит а не rtk-query або createAsyncThunk для того, щоб працювала індексація сайту (запит відбувається на стороні сервера завдяки цьому, а не на стороні клієнта) + реалізовано кешування даних
 	const res = await fetch(`${process.env.BASE_URL}products`, { cache: 'force-cache' });
