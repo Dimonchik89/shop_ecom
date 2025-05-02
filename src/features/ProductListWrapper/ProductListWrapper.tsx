557ab6b573f7105b1f129e44bb573a971d68206e
Dmitriy Kislitsyn <dima.music1989@gmail.com>
2025-05-02 08:21:39 +0300

    add env file

--- src/features/ProductListWrapper/ProductListWrapper.tsx
+++ src/features/ProductListWrapper/ProductListWrapper.tsx
@@ -6,7 +6,7 @@ import ProductList from '../ProductList/ProductList';
 // обгортка в якій отримуємо дані із сервера. ззаду для правильної роботи компонента Suspense
 const ProductListWrapper = async () => {
 	// надсилання запиту для отримання продуктiв. Використовую звичайний fetch запит а не rtk-query або createAsyncThunk для того, щоб працювала індексація сайту (запит відбувається на стороні сервера завдяки цьому, а не на стороні клієнта) + реалізовано кешування даних
-	const res = await fetch('http://localhost:3001/api/products', { cache: 'force-cache' });
+	const res = await fetch(`${process.env.BASE_URL}products`, { cache: 'force-cache' });
 	if (!res.ok) {
 		return <Error message={`${res.status}`} />;
 	}
--- src/features/ProductPage/ProductPage.tsx
+++ src/features/ProductPage/ProductPage.tsx
@@ -10,7 +10,7 @@ interface ProductPageProps {
 
 const ProductPage = async ({ productId }: ProductPageProps) => {
 	// надсилання запиту для отримання одного продукту. Використовую звичайний fetch запит а не rtk-query або createAsyncThunk для того, щоб працювала індексація сайту (запит відбувається на стороні сервера завдяки цьому, а не на стороні клієнта) + реалізовано кешування даних
-	const res = await fetch(`http://localhost:3001/api/product/${productId}`, { cache: 'force-cache' });
+	const res = await fetch(`${process.env.BASE_URL}product/${productId}`, { cache: 'force-cache' });
 	if (!res.ok) {
 		return <Error message={`${res.status}`} />;
 	}
